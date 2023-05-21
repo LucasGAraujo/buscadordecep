74ab4f03b3273e24c84dc12a4b26ba0fd22e4fce
Lucas Gomes <lucas.garaujo@al.infnet.edu.br>
2023-05-21 18:52:39 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Endereco from '../src/Endereco.js';
+
+
+function App() {
+  return (
+    <div>
+      <Endereco/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Endereco.js
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+
+function Endereco() {
+  const [endereco, setEndereco] = useState('');
+  const [dadosEndereco, setDadosEndereco] = useState(null);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try{
+      const response =await fetch(`https://viacep.com.br/ws/${endereco}/json/`)
+      const data = await response.json();
+      setDadosEndereco(data)
+    }catch(error){
+      console.log("Errorrrrrr helpsme" + error)
+    }
+      
+  };
+
+  return (
+    <div>
+      <div className='Cep'>
+        <form onSubmit={handleSubmit}>
+          <input
+            type='text'
+            value={endereco}
+            placeholder='Digite seu Cep'
+            onChange={(e) => setEndereco(e.target.value)}
+          />
+          <button type="submit">Buscar</button>
+        </form>
+        {dadosEndereco && (
+          <div className="end">
+            <h2>Seu Cep:</h2>
+            <p>{dadosEndereco.cep}</p>
+            <h2>Logradouro:</h2>
+            <p>{dadosEndereco.logradouro}</p>
+            <h2>Bairro:</h2>
+            <p>{dadosEndereco.bairro}</p>
+            <h2>Localidade:</h2>
+            <p>{dadosEndereco.localidade}</p>
+            <h2>UF:</h2>
+            <p>{dadosEndereco.uf}</p>
+            <h2>IBGE:</h2>
+            <p>{dadosEndereco.ibge}</p>
+            <h2>GIA:</h2>
+            <p>{dadosEndereco.gia}</p>
+            <h2>SIAFI:</h2>
+            <p>{dadosEndereco.siafi}</p>
+            <h2>DDD:</h2>
+            <p>{dadosEndereco.ddd}</p>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default Endereco;
